98c9887fe9373b4859085dce6e044a8262d6efec
adityarawat1337x <napster98766@gmail.com>
2021-06-16 01:34:29 +0530

    backend done

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import VideoPlayer from "./components/VideoPlayer"
+import Options from "./components/Options"
+import Notify from "./components/Notify"
+
+const App = () => {
+    return (
+        <>
+            <div>
+                <h1> Video Chat</h1>
+            </div>
+            <VideoPlayer/>
+            <Notify/>
+        </>
+    )
+}
+
+export default App
